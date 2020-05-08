cb19503aeb02549bf9d70a38c29aa92272c5d0df
unknown <macjay314@gmail.com>
2020-05-08 11:34:08 -0700

    Add receipt output for console. Need to integrate bread and pastry price

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using System.Drawing;
 using Console = Colorful.Console;
 using Bakery.BakedGoods;
@@ -12,20 +11,37 @@ namespace Bakery
     {
       Bread bread = new Bread();
       Pastry pastry = new Pastry();
+      Console.WriteLine("console in pink", Color.Teal);
       Console.WriteLine(@"Hello! Welcome to Pierre's Bakery. We have a few choice items for sale:
 Bread  $5   Sale: buy 2 get 1 free!
 Pastry $2   Sale: buy 3 for $5", Color.Green);
       Console.WriteLine("How much bread would you like to purchase?");
       string bString = Console.ReadLine();
-      if (int.TryParse(bString, out int bInt))
-      {
-      } else {bInt = 0;}
+      if (!int.TryParse(bString, out int bInt)){bInt = 0;}
       Console.WriteLine("How many pastries would you like?");
       string pString = Console.ReadLine();
-      if (int.TryParse(pString, out int pInt))
-      {
-      } else {pInt = 0;}
-      Console.WriteLine("Bread: " + bread.bPrice(bInt) + " Pastries: " + pastry.pPrice(pInt));
+      if (!int.TryParse(pString, out int pInt)){pInt = 0;} 
+      Console.WriteLine(@"+-------------------------------------+
+|                                     |
+|          Pierre's Bakery            |
+|                                     |
+|            Candy Lane               |
+|         Seattle Washington          |
+|           1-800(BAKED)              |
+|                                     |
+|  Bread                              |
+|  Pastry(ies)                        |
+|                                     |
+|  Subtotal                  12.04    |
+|                                     |
+|  TOTAL                     12.04    |
+|  Cash                      15.04    |
+|  Change Due                 3.00    |
+|                                     |
+|  Item Count:  5                     |
+|                                     |
+|             THANK YOU!         :F_P:|
++-------------------------------------+");
     }
   }
 }
